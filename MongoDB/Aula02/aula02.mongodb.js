d948f819fd7b817aaa92f6a9c27afd7be3c3a015
Ryan <ryanjobs234@gmail.com>
2025-02-17 22:14:38 -0300

    Finalização da Aula

--- MongoDB/Aula02/aula02.mongodb.js
+++ MongoDB/Aula02/aula02.mongodb.js
@@ -56,8 +56,63 @@ use('estoque')
 db.getCollectionInfos({ name: 'produtos' })
 
 use('estoque')
-try{
-db.produtos.insertOne({ abobrinha: "tem" })
-}catch(err){
+try {
+    db.produtos.insertOne({ abobrinha: "tem" })
+} catch (err) {
     printjson(err)
 }
+
+use('estoque')
+db.estados.insertMany([
+    { sigla: 'SP', nome: 'São Paulo', populacao: 12000000 },
+    { sigla: 'AC', nome: 'Acre', populacao: 712000 },
+    { sigla: 'RJ', nome: 'Rio de Janeiro', populacao: 2500000 }
+])
+
+use('estoque')
+db.estados.find(
+    {}, //Filtros
+    {}, //Atributos a serem exibidos
+)
+
+use('estoque')  //select id, nome from estados where sigla='SP'
+db.estados.find({ sigla: { $eq: 'SP' } }, { _id: 0, nome: 1 })
+
+use('estoque')  //select id, nome from estados where sigla!='SP'
+db.estados.find({ sigla: { $ne: 'SP' } }, { _id: 0, nome: 1 })
+
+use('estoque')  //i = insensitive case
+db.estados.find({ nome: /paulo/i }, { _id: 0, nome: 1 })
+
+use('estoque')  //$ = que termine com
+db.estados.find({ nome: /$paulo/i }, { _id: 0, nome: 1 })
+
+use('estoque')  //^ = que começe com
+db.estados.find({ nome: /^paulo/i }, { _id: 0, nome: 1 })
+
+use('estoque')  //select * from estados where populacao >= 11000000
+db.estados.find({ populacao: { $gte: 11000000 } })
+
+use('estoque')  //select * from estados where sigla in ('AC', 'RJ')
+db.estados.find({ sigla: { $in: ['AC', 'RJ'] } })
+
+use('estoque')  //select * from estados where sigla not in ('AC', 'RJ')
+db.estados.find({ sigla: { $nin: ['AC', 'RJ'] } })
+
+use('estoque')  //select * from estados where sigla = 'RJ' or sigla = 'AC'
+db.estados.find({ $or: [{ sigla: { $eq: 'RJ' } }, { sigla: { $eq: 'AC' } }] }, { _id: 0, nome: 1, sigla: 1 })
+
+use('estoque')  //delete from estados where sigla = 'AC'
+db.estados.deleteOne({ sigla: { $eq: 'AC' } })
+
+use('estoque')  //delete from estados where character = 'o'
+db.estados.deleteMany({ nome: /o/i })
+
+use('estoque')  //update from estados set populacao = 1000
+db.estados.updateOne({ sigla: { $eq: 'AC' } }, { $set: { populacao: 1000 } })
+
+use('estoque')  //select * from estados where sigla = 'AC'
+db.estados.find({ sigla: { $eq: 'AC' } })
+
+use('estoque')  //update from estados set pais = 'Brasil'
+db.estados.updateMany({}, { $set: { pais: 'Brasil' } })
