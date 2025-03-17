7c78125a575fa595b7911a9edd8b0b18b1b016dd
Ryan <ryanjobs234@gmail.com>
2025-03-17 20:38:52 -0300

    Mais parametros de pesquisa no find usando a estrutura do GeoJason

--- MongoDB/Aula05/geo.mongodb.js
+++ MongoDB/Aula05/geo.mongodb.js
@@ -14,17 +14,37 @@ db.estados.find().forEach(function (estado) {
         }
     )
 })
-/*
-  {
-    "_id": {
-      "$oid": "67cf6d0c04b4e1387ad9caf8"
-    },
-    "codigo_uf": 11,
-    "uf": "RO",
-    "nome": "Rondônia",
-    "latitude": -10.83,
-    "longitude": -63.34
-  }
-*/
+
 use('estoque')
-db.estados.find({},{local:1})
+db.estados.find({}, { local: 1 })
+
+//Criando o indece 2dSphere
+use('estoque')
+db.estados.createIndex({ local: "2dsphere" })
+
+//Localizando os estados até 200km  próximos da Fatec
+use('estoque')
+db.estados.find({
+    local: {
+        $near: {
+            $geometry: {
+                type: "Point",
+                coordinates: [-47.4495, -23.5313]   //Fatec
+            },
+            $maxDistance: 2000000 //Em metros (200km)
+        }
+    }
+}, { nome: 1, _id: 0 })
+
+use('estoque')
+db.estados.find({
+    local: {
+        $geoWithin: {
+            $centerSphere:
+                [
+                    [-47.4495, -23.5313], //Fatec
+                    2000 / 6378.1 //Raio em radianos
+                ]
+        }
+    }
+}, { nome: 1, _id: 0 })
